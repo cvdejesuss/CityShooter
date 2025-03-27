cb2939abe586057b2599a7ae5e2c08e3f529710f
Carlos <cvdejesuss.bit@gmail.com>
2025-03-27 13:41:53 -0300

    Creating asset folder Inserting all assets Creating the menu Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_PINK = (255, 182, 193)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
