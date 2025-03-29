f1ed46b3ead1ce8bd22e2081b8cd1181cf539ac7
Carlos <cvdejesuss.bit@gmail.com>
2025-03-29 01:02:06 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/Const.py
+++ code/Const.py
@@ -7,20 +7,31 @@ COLOR_YELLOW = (255, 255, 0)
 
 # E
 ENTITY_SPEED = {
-    'Level1Bg0':1,
-    'Level1Bg1':1,
-    'Level1Bg2':2,
-    'Level1Bg3':3,
-    'Level1Bg4':4,
-    'Level1Bg5':5,
-    'Player1' :3,
-    'Player2' :3,
-    'Enemy1' : 2,
-    'Enemy2' : 1,
+    'Level1Bg0': 1,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 EVENT_ENEMY = pygame.USEREVENT + 1
 
-
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 30,
+    'Enemy2': 40,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -30,17 +41,16 @@ MENU_OPTION = ('NEW GAME 1P',
                'EXIT')
 
 # P
-PLAYER_KEY_UP = {'Player1':pygame.K_w,
-                 'Player2':pygame.K_UP}
-PLAYER_KEY_DOWN = {'Player1':pygame.K_s,
-                 'Player2':pygame.K_DOWN}
-PLAYER_KEY_LEFT = {'Player1':pygame.K_a,
-                 'Player2':pygame.K_LEFT}
-PLAYER_KEY_RIGHT = {'Player1':pygame.K_d,
-                 'Player2':pygame.K_RIGHT}
-PLAYER_KEY_SHOOT = {'Player1':pygame.K_LCTRL,
-                 'Player2':pygame.K_RCTRL}
-
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                 'Player2': pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                   'Player2': pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                   'Player2': pygame.K_LEFT}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
+                    'Player2': pygame.K_RIGHT}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
+                    'Player2': pygame.K_RCTRL}
 
 # S
 SPAWN_TIME = 4000
